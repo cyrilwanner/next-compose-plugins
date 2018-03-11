4eeed2a276427e1cf6d018434654929dd8415f47
Cyril Wanner <cyril.wanner@hotmail.com>
2018-03-11 15:56:26 +0100

    Create the plugin

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = ([ ...plugins ], nextConfig) => {
+  let config = { ...nextConfig };
+
+  plugins.forEach((plugin) => {
+    if (plugin instanceof Array) {
+      const [ initPlugin, pluginConfig ] = plugin;
+      config = initPlugin({ ...config, ...(pluginConfig || {}) });
+    } else {
+      config = plugin(config);
+    }
+  });
+
+  return config;
+};
