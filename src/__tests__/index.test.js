ef9361ee5dd401f9699412728b1e1f6bd0e7c441
Cyril Wanner <cyril.wanner@hotmail.com>
2018-04-07 23:26:12 +0200

    Add code linting (#5)

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,5 +3,5 @@ import 'jest';
 describe('next-compose-plugins', () => {
   it('runs tests', () => {
     expect(1).toEqual(1);
-  })
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-module.exports = ([ ...plugins ], nextConfig = {}) => {
+module.exports = ([...plugins], nextConfig = {}) => {
   let config = { ...nextConfig };
 
   plugins.forEach((plugin) => {
     if (plugin instanceof Array) {
-      const [ initPlugin, pluginConfig ] = plugin;
+      const [initPlugin, pluginConfig] = plugin;
       config = initPlugin({ ...config, ...(pluginConfig || {}) });
     } else {
       config = plugin(config);
